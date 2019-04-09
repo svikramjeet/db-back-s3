c061d25471ec498421ab486c5d946d59d47d0cbc
vikram-ucreate <vikramjeet@ucreate.co.in>
2019-04-09 15:15:16 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,34 @@
+## Heroku Buildpack: heroku-db-backup-s3
+Capture DB backup & copy to s3 bucket.
+
+### Installation
+Add buildpack to your Heroku app
+```
+heroku buildpacks:add https://github.com/svikramjeet/db-back-s3 --app <your_app>
+```
+> Buildpacks are scripts that are run when your app is deployed.
+
+### Configure environment variables
+```
+heroku config:add AWS_ACCESS_KEY_ID=XXX --app <your_app>
+heroku config:add AWS_SECRET_ACCESS_KEY=YYY --app <your_app>
+heroku config:add AWS_DEFAULT_REGION=eu-west-1 --app <your_app>
+heroku config:add S3_BUCKET_PATH=bucket_name --app <your_app>
+
+```
+Go to settings page of your Heroku application and add Config Var `DBURL_FOR_BACKUP` with the same value as var `DATABASE_URL`. This is our DB connection string.
+
+### Scheduler
+Add addon scheduler to your app. 
+```
+heroku addons:create scheduler --app <your_app>
+```
+Create scheduler.
+```
+heroku addons:open scheduler --app <your_app>
+```
+Now in browser `Add new Job`.
+Paste next line:
+`bash /app/vendor/backup.sh -db <somedbname>`
+and configure FREQUENCY. Paramenter `db` is used for naming convention when we create backups. We don't use it for dumping  database with the same name.
+
---
+++ backup.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+
+DBNAME=""
+EXPIRATION="30"
+Green='\033[0;32m'
+EC='\033[0m' 
+FILENAME=`date +%H_%M_%d%m%Y`
+
+# terminate script on any fails
+set -e
+
+while [[ $# -gt 1 ]]
+do
+key="$1"
+
+case $key in
+    -exp|--expiration)
+    EXPIRATION="$2"
+    shift
+    ;;
+    -db|--dbname)
+    DBNAME="$2"
+    shift
+    ;;
+esac
+shift
+done
+
+if [[ -z "$DBNAME" ]]; then
+  echo "Missing DBNAME variable"
+  exit 1
+fi
+if [[ -z "$AWS_ACCESS_KEY_ID" ]]; then
+  echo "Missing AWS_ACCESS_KEY_ID variable"
+  exit 1
+fi
+if [[ -z "$AWS_SECRET_ACCESS_KEY" ]]; then
+  echo "Missing AWS_SECRET_ACCESS_KEY variable"
+  exit 1
+fi
+if [[ -z "$AWS_DEFAULT_REGION" ]]; then
+  echo "Missing AWS_DEFAULT_REGION variable"
+  exit 1
+fi
+if [[ -z "$S3_BUCKET_PATH" ]]; then
+  echo "Missing S3_BUCKET_PATH variable"
+  exit 1
+fi
+if [[ -z "$DATABASE_URL" ]]; then
+  echo "Missing DATABASE_URL variable"
+  exit 1
+fi
+
+printf "${Green}Start dump${EC}"
+# Maybe in next 'version' use heroku-toolbelt
+# /app/vendor/heroku-toolbelt/bin/heroku pg:backups capture $DATABASE --app $HEROKU_TOOLBELT_APP
+# BACKUP_URL=`/app/vendor/heroku-toolbelt/bin/heroku pg:backups:public-url --app $HEROKU_TOOLBELT_APP | cat`
+# curl --progress-bar -o /tmp/"${DBNAME}_${FILENAME}" $BACKUP_URL
+# gzip /tmp/"${DBNAME}_${FILENAME}"
+
+time pg_dump $DATABASE_URL | gzip >  /tmp/"${DBNAME}_${FILENAME}".gz
+
+#EXPIRATION_DATE=$(date -v +"2d" +"%Y-%m-%dT%H:%M:%SZ") #for MAC
+EXPIRATION_DATE=$(date -d "$EXPIRATION days" +"%Y-%m-%dT%H:%M:%SZ")
+
+printf "${Green}Move dump to AWS${EC}"
+time /app/vendor/awscli/bin/aws s3 cp /tmp/"${DBNAME}_${FILENAME}".gz s3://$S3_BUCKET_PATH/$DBNAME/"${DBNAME}_${FILENAME}".gz --expires $EXPIRATION_DATE
+
+# cleaning after all
+rm -rf /tmp/"${DBNAME}_${FILENAME}".gz
