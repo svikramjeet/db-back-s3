f88bddca425acefc275f9200c9db6ceb97dc71cd
Vikramjeet SIngh <vikramjeet@ucreate.co.in>
2019-04-18 17:09:24 +0530

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -66,7 +66,7 @@ time pg_dump -b -F c --dbname=$DATABASE_URL >  /tmp/"${DBNAME}_v_${FILENAME}".gz
 EXPIRATION_DATE=$(date -d "$EXPIRATION days" +"%Y-%m-%dT%H:%M:%SZ")
 
 printf "${Green}Move dump to AWS${EC}"
-time /app/vendor/awscli/bin/aws s3 cp /tmp/"${DBNAME}_${FILENAME}".gz s3://$S3_BUCKET_PATH/$DBNAME/"${DBNAME}_${FILENAME}".gz --expires $EXPIRATION_DATE
-
+#time /app/vendor/awscli/bin/aws s3 cp /tmp/"${DBNAME}_${FILENAME}".gz s3://$S3_BUCKET_PATH/$DBNAME/"${DBNAME}_${FILENAME}".gz --expires $EXPIRATION_DATE
+time /app/vendor/awscli/bin/aws s3 cp /tmp/"${DBNAME}_v_${FILENAME}" s3://$S3_BUCKET_PATH/$DBNAME/"${DBNAME}_${FILENAME}" --expires $EXPIRATION_DATE
 # cleaning after all
 rm -rf /tmp/"${DBNAME}_${FILENAME}".gz
