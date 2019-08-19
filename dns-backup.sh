7f6230bc35ca70db78cc20dd5b884c3fcebaa8ef
svikramjeet <vikramjeet@ucreate.co.in>
2019-08-19 18:59:22 +0530

    added backuo for dns

---
+++ dns-backup.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+FNAME="heroku-env-backup"
+EXPIRATION="30"
+Green='\033[0;32m'
+EC='\033[0m' 
+FILENAME=`date +%H_%M_%d%m%Y`
+sourceApp="$1"
+
+# terminate script on any fails
+set -e
+
+printf "${Green}Start env-dump${EC}"
+
+dig brewbroker.com any +noall +answer | gzip >  /tmp/"${FNAME}_${FILENAME}".gz
+
+
+EXPIRATION_DATE=$(date -d "$EXPIRATION days" +"%Y-%m-%dT%H:%M:%SZ")
+
+printf "${Green}Move dns-backup to AWS${EC}"
+time /app/vendor/awscli/bin/aws s3 cp /tmp/"${FNAME}_${FILENAME}".gz s3://$S3_BUCKET_PATH/$FNAME/"${FNAME}_${FILENAME}".gz --expires $EXPIRATION_DATE
+
+# cleaning after all
+rm -rf /tmp/"${FNAME}_${FILENAME}".gz
---
+++ env-backup.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+FNAME="om-env-backup"
+EXPIRATION="30"
+Green='\033[0;32m'
+EC='\033[0m' 
+FILENAME=`date +%H_%M_%d%m%Y`
+sourceApp="$1"
+
+# terminate script on any fails
+set -e
+
+printf "${Green}Start env-dump${EC}"
+
+#download heroku cli
+# lgin to cli
+
+time heroku config -s -a "$sourceApp"  | gzip >  /tmp/"${FNAME}_${FILENAME}".gz
+
+EXPIRATION_DATE=$(date -d "$EXPIRATION days" +"%Y-%m-%dT%H:%M:%SZ")
+
+printf "${Green}Move env-backup to AWS${EC}"
+time /app/vendor/awscli/bin/aws s3 cp /tmp/"${FNAME}_${FILENAME}".gz s3://$S3_BUCKET_PATH/$FNAME/"${FNAME}_${FILENAME}".gz --expires $EXPIRATION_DATE
+
+# cleaning after all
+rm -rf /tmp/"${FNAME}_${FILENAME}".gz
