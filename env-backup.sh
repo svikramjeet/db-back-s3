33cf46378d39d664cd5dd8c2d903b4fc63991dc4
svikramjeet <vikramjeet@ucreate.co.in>
2019-08-20 09:34:15 +0530

    updated env backup file

--- env-backup.sh
+++ env-backup.sh
@@ -1,28 +1,21 @@
 #!/bin/bash
 
-FNAME="om-env-backup"
+FNAME="heroku-env-backup"
 EXPIRATION="30"
 Green='\033[0;32m'
 EC='\033[0m' 
 FILENAME=`date +%H_%M_%d%m%Y`
-<<<<<<< HEAD
 sourceApp="$1"
-=======
->>>>>>> c32e692385cc120bc5f27d6bd7406e78308f2c83
 
 # terminate script on any fails
 set -e
 
 printf "${Green}Start env-dump${EC}"
 
-<<<<<<< HEAD
 #download heroku cli
 # lgin to cli
 
 time heroku config -s -a "$sourceApp"  | gzip >  /tmp/"${FNAME}_${FILENAME}".gz
-=======
-time pg_dump $DATABASE_URL | gzip >  /tmp/"${FNAME}_${FILENAME}".gz
->>>>>>> c32e692385cc120bc5f27d6bd7406e78308f2c83
 
 EXPIRATION_DATE=$(date -d "$EXPIRATION days" +"%Y-%m-%dT%H:%M:%SZ")
 
