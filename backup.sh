90a854a7443f5660e007add9c98ec13cd3cf03dd
Vikramjeet Singh <vikramjeet@ucreate.co.in>
2021-03-10 20:27:22 +0530

    awscliv2 support

--- backup.sh
+++ backup.sh
@@ -52,21 +52,12 @@ if [[ -z "$DATABASE_URL" ]]; then
 fi
 
 printf "${Green}Start dump${EC}"
-# Maybe in next 'version' use heroku-toolbelt
-# /app/vendor/heroku-toolbelt/bin/heroku pg:backups capture $DATABASE --app $HEROKU_TOOLBELT_APP
-# BACKUP_URL=`/app/vendor/heroku-toolbelt/bin/heroku pg:backups:public-url --app $HEROKU_TOOLBELT_APP | cat`
-# curl --progress-bar -o /tmp/"${DBNAME}_${FILENAME}" $BACKUP_URL
-# gzip /tmp/"${DBNAME}_${FILENAME}"
 
-#time pg_dump $DATABASE_URL | gzip >  /tmp/"${DBNAME}_${FILENAME}".gz
 time pg_dump -b -F c --dbname=$DATABASE_URL | gzip >  /tmp/"${DBNAME}_${FILENAME}".gz
-#time pg_dump -b -F c --dbname=$DATABASE_URL >  /tmp/"${DBNAME}_v_${FILENAME}".gz
 
-#EXPIRATION_DATE=$(date -v +"2d" +"%Y-%m-%dT%H:%M:%SZ") #for MAC
 EXPIRATION_DATE=$(date -d "$EXPIRATION days" +"%Y-%m-%dT%H:%M:%SZ")
 
 printf "${Green}Move dump to AWS${EC}"
-time /app/vendor/awscli/bin/aws s3 cp /tmp/"${DBNAME}_${FILENAME}".gz s3://$S3_BUCKET_PATH/$DBNAME/"${DBNAME}_${FILENAME}".gz --expires $EXPIRATION_DATE
-#time /app/vendor/awscli/bin/aws s3 cp /tmp/"${DBNAME}_v_${FILENAME}" s3://$S3_BUCKET_PATH/$DBNAME/"${DBNAME}_${FILENAME}" --expires $EXPIRATION_DATE
+time /tmp/bin/aws s3 cp /tmp/"${DBNAME}_${FILENAME}".gz s3://$S3_BUCKET_PATH/$DBNAME/"${DBNAME}_${FILENAME}".gz --expires $EXPIRATION_DATE
 # cleaning after all
 rm -rf /tmp/"${DBNAME}_${FILENAME}".gz
